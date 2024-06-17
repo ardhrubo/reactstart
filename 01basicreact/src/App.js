1707422825358e3e15521b1d2814b58cc0b6ed74
ardhrubo <ardhrubo3038@gmail.com>
2024-06-17 23:09:27 +0600

    foundation

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,13 @@
 
+import Youtube from "./Youtube.jsx";
+
 function App() {
   return (
+    <>
    
       <h1> Basic React App  </h1>
-
+      <Youtube />
+    </>
   );
 }
 
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,13 @@
+
+
+function Youtube() {
+
+    return (
+        <h1> Youtube React app </h1>
+    )
+
+}
+
+
+export default Youtube;
+
