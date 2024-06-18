ff3e17b78408764a7267784d6f5569a24c8149b1
ardhrubo <ardhrubo3038@gmail.com>
2024-06-18 12:16:45 +0600

    custom react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -2,11 +2,14 @@
 import Youtube from './Youtube';
 
 function App() {
+
+  const username = "Dhrubo"
+
   return (
     <>
     
     <h1> Vite Basic App</h1>
-    <h1> Vite Basic App</h1>
+    <h1> Vite Basic App {username}</h1>
     <Youtube />
 
 
