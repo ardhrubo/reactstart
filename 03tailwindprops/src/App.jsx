d58f05e08a01ecd43eab3498183e6ec891e440b3
ardhrubo <ardhrubo3038@gmail.com>
2024-06-19 12:47:19 +0600

    tailwind css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <div className='text-3xl bg-green-500 p-3 rounded-md'> Vite with tailwindcss</div>
+      
+    </>
+  )
+}
+
+export default App
+
+
+
+
