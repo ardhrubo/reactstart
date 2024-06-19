e479f46c040698516f3b77656c2be45ab63defcf
ardhrubo <ardhrubo3038@gmail.com>
2024-06-19 17:00:26 +0600

    new update

---
+++ 04colorpalate/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <h1 className='bg-emerald-500 text-3xl'> A  bacground color changer app </h1>
+    
+
+    </>
+  )
+}
+
+export default App
