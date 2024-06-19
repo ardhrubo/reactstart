cee4824ff28efeb98967c61f4d891bcda7e02968
ardhrubo <ardhrubo3038@gmail.com>
2024-06-19 18:55:53 +0600

    color palate project

--- 04colorpalate/src/App.jsx
+++ 04colorpalate/src/App.jsx
@@ -5,14 +5,24 @@ import './App.css'
 
 function App() {
 
+  const [color, setColor] = useState('indigo')
+
+
 
   return (
-    <>
-
-    <h1 className='bg-emerald-500 text-3xl'> A  bacground color changer app </h1>
+ 
+    <div className='w-full h-screen bg-black duration-200' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button onClick={()=> setColor('red')} className='outline-none px-4 py-1 bg-red-600 rounded-full shadow-lg text-white font-bold '>Red</button>
+          <button onClick={()=> setColor('green')} className='outline-none px-4 py-1 bg-green-600 rounded-full shadow-lg text-white font-bold '>Green</button>
+          <button onClick={()=> setColor('blue')}  className='outline-none px-4 py-1 bg-blue-600 rounded-full shadow-lg text-white font-bold '>Blue</button>
+        </div>
+      
+      </div>
     
+    </div>
 
-    </>
   )
 }
 
